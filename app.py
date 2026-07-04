4e4912b0b1e2ff7ddbfcdca9325c8f17865565bb
rogerepsilva <rogerepsilva@gmail.com>
2026-07-03 21:05:29 -0300

    versao 0.2

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ idade_tres = 30.5
 
 #Type retorna o Tipo da variável.
 print(f"{type(idade)}\n{type(idade_dois)}\n{type(idade_tres)}")
+
+#Testando o git hub
+print("Encerrando a primeira semana do curso")
